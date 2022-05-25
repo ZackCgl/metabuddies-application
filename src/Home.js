2014cf1bf93e50e35b87e34026b3ac705e5a8068
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-05-25 19:09:21 +0200

    final

--- src/Home.js
+++ src/Home.js
@@ -3,49 +3,46 @@ import "./Home.css"
 import Product from './Product'
 import FlipMove from 'react-flip-move';
 import Rainbowman from "./images/Rainbowman.png"
-import btcboy2 from "./images/btcboy2.png"
-import btcboy3 from "./images/btcboy3.png"
-import btcboy5 from "./images/btcboy5.png"
-import bannerhome from './images/bannerhome.png'
+
 
 function Home() {
   return (
     <div className='home'>
         <div className='home__container'>
-            <img className='home__image' src={bannerhome} alt=""/>
+            <img className='home__image' src="https://wallpaperaccess.com/full/7087237.jpg" alt=""/>
         
         <div className='home__row'>
         
         <Product 
         id="929292"
         title="Papa Buddy 3d printed Home character"
-        price={299.99}
-        image={Rainbowman}
-        rating={3}
+        price={99.99}
+        image="https://i.ibb.co/rcWPkHN/Rainbowman.png"
+        rating={1}
         />
         
         <Product
          id="929293"
          title="Heisenberg 3d printed Home character"
-        price={299.99}
-        image={btcboy3}
-        rating={4}
+        price={149.99}
+        image="https://i.ibb.co/rcWPkHN/Rainbowman.png"
+        rating={2}
         />
 
   	    <Product
-         id="929293"
+         id="929294"
          title="Party Buddie 3d printed Home character"
-        price={299.99}
-        image={btcboy5}
-        rating={3}
+        price={199.99}
+        image="https://i.ibb.co/rcWPkHN/Rainbowman.png"
+        rating={4}
         />
 
         <Product
-         id="929293"
-         title="Bitcoin Buddie 3d printed Home character"
-        price={299.99}
-        image={btcboy2}
-        rating={5}
+         id="929295"
+         title="Party Buddie 3d printed Home character"
+        price={12.99}
+        image="https://i.ibb.co/rcWPkHN/Rainbowman.png"
+        rating={4}
         />
         
         </div>
